f9eac6f536baf163cbd7b49fa6d16de2818f562a
Yuku on yuku12 <yukuku@gmail.com>
2024-05-03 14:38:29 +0800

    Jangan jatoh

--- naskah/lendir.gd
+++ naskah/lendir.gd
@@ -6,15 +6,29 @@ var arah = 1
 
 @onready var pancar_sinar_kanan = $"Pancar sinar kanan"
 @onready var pancar_sinar_kiri = $"Pancar sinar kiri"
+
+@onready var sensor_lantai_kanan = $"Sensor lantai kanan"
+@onready var sensor_lantai_kiri = $"Sensor lantai kiri"
+
+
 @onready var animated_sprite = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# awas nabrak
 	if pancar_sinar_kanan.is_colliding():
 		arah = -1
-		animated_sprite.flip_h = true
 	if pancar_sinar_kiri.is_colliding():
 		arah = +1
-		animated_sprite.flip_h = false
+		
+	# awas jatoh
+	print(sensor_lantai_kanan.is_colliding())
+	if not sensor_lantai_kanan.is_colliding():
+		arah = -1
+	if not sensor_lantai_kiri.is_colliding():
+		arah = +1
+		
+	animated_sprite.flip_h = arah != 1
+		
 	position.x += arah * LAJU * delta
 	
