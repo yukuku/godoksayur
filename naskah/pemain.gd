b8b81c4ba59bf6d0dbe21567cdb4361cd375eb03
Yuku on yuku12 <yukuku@gmail.com>
2024-05-07 14:21:18 +0800

    Double jump

--- naskah/pemain.gd
+++ naskah/pemain.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var uda_loncat = 0
+
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
@@ -14,9 +16,14 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("loncat") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Double jump
+	if is_on_floor():
+		uda_loncat = 0
+
+	if Input.is_action_just_pressed("loncat"):
+		if uda_loncat < 2:
+			velocity.y = JUMP_VELOCITY
+			uda_loncat += 1
 
 	var direction = Input.get_axis("gerak_kiri", "gerak_kanan")
 	
